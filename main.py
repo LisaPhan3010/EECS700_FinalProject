b0d0a5ab19e0db6b95c2cb8fec33e2e860d77077
Gavin Billinger <gbillinger@ku.edu>
2025-11-17 21:43:21 -0600

    Filler inputs
    Added filler inputs in place of feature extraction for now.

--- main.py
+++ main.py
@@ -2,6 +2,15 @@ import numpy as np
 from binarization import binarize, build_score_matrix
 
 def main():
+	# Filler inputs for now
+	gabor_P5 = np.random.randn(128)
+	gabor_P6 = np.random.randn(128)
+	gabor_P7 = np.random.randn(128)
+
+	gabor_G1 = np.random.randn(128)
+	gabor_G2 = np.random.randn(128)
+	gabor_G3 = np.random.randn(128)
+	gabor_G4 = np.random.randn(128)
 	# Step 1: Likely some file I/O stuff.
 	
 	# Step 2: Feature extraction	
